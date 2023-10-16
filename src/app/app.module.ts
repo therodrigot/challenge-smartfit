f50d28ca67fcaf2042dc6fa334b297c7cfcfe661
Rodrigo Teixeira <rodrigo@navas.design>
2023-10-16 15:20:28 -0300

    feat: base setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
