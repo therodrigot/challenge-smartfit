49d76ed4fce653fd7c3ac377a5c814fb1ba135ac
Rodrigo Teixeira <rodrigo@navas.design>
2023-10-16 15:47:10 -0300

    feat: form structure

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
-  results = []
+  results = [];
+  formGroup!: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      periodo: '',
+      showClosed: false,
+    });
+  }
+
+  onSubmit(): void {
+    console.log("submit",this.formGroup.value);
+  }
+
+  onClean(): void {
+    console.log('clean');
+    this.formGroup.reset();
+  }
 }
