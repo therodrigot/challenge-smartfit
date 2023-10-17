60a70c09d953f49eb2619c75871338a988e30a7c
Rodrigo Teixeira <rodrigo@navas.design>
2023-10-17 17:56:34 -0300

    feat: cardlist and card components

---
+++ src/app/components/cards-list/cards-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-list',
+  templateUrl: './cards-list.component.html',
+  styleUrls: ['./cards-list.component.scss']
+})
+export class CardsListComponent {
+  @Input() unitsList: Location[] = [];
+}
